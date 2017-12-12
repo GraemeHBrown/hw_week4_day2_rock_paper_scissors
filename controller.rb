15221541dd51696ded8ca7e75dd304728d339baa
Graeme Brown <graemehbrown@googlemail.com>
2017-12-12 16:51:05 +0000

    Added Game class and test class for Game.

--- controller.rb
+++ controller.rb
@@ -1,10 +1,14 @@
 require('sinatra')
 require('sinatra/contrib/all')
 require('pry-byebug')
+require_relative('./models/game.rb')
 
 get('/') do
   erb(:home)
 end
 
 
-get('/')
+get('/:move1/:move2') do
+  game = Game.new(params[:move1], params[:move2])
+  @result = game.play()
+end
